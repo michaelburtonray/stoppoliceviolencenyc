9d3e3b0f664b7972b3513767cc25791e37d603c7
michaelburtonray <michael@human-nyc.com>
2020-07-08 18:12:16 -0400

    side margins for embed row

--- components/EmbedCodeRow.js
+++ components/EmbedCodeRow.js
@@ -1,19 +1,21 @@
 export default function EmbedCodeRow({fields}) {
-  const { embedCode, aspectRatio } = fields;
+  const { embedCode, aspectRatio, isFullBleed  } = fields;
 
 
 
 
   return (
-    <div className="embed-code-row wrapper">
+    <div className={`embed-code-row wrapper ${isFullBleed ? 'full-bleed' : ''}`}>
 
       {aspectRatio && <svg className="iframe-spacer" xmlns='http://www.w3.org/2000/svg'  viewBox={`0 0 ${aspectRatio.split('x')[0]} ${aspectRatio.split('x')[1]}`}></svg>}
 
       <div className="embed-wrapper" dangerouslySetInnerHTML={{ __html: embedCode }} />
       <style jsx global>{`
         .embed-code-row {
-          padding: 0;
           position: relative;
+        }
+        .embed-code-row.full-bleed {
+          padding: 0;
         }
         .embed-code-row iframe {
           border: none;
@@ -31,6 +33,10 @@ export default function EmbedCodeRow({fields}) {
           right: 0;
           bottom: 0;
           left: 0;
+          margin: 0 var(--gutter);
+        }
+        .full-bleed .iframe-spacer + .embed-wrapper {
+          margin: 0;
         }
 
         .iframe-spacer + .embed-wrapper iframe {
